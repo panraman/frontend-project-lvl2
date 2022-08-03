50b767a9b61d719a568eb6ce362c186f7bc004f2
panraman <gaevskiy12@gmail.com>
2022-08-04 02:48:36 +0300

    fix errors with tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,22 +7,26 @@ import genDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => resolve(__dirname, '..', '__fixtures__', 'fortest', filename);
 const readFile = (file) => fs.readFileSync(getFixturePath(file), 'utf8');
-const fullFileJson = getFixturePath('file1.json');
-const emptyFileJson = getFixturePath('emptyFile.json');
+const full1json = getFixturePath('full1.json');
+const full2json = getFixturePath('full2.json');
+const emptyFileJson = getFixturePath('empty.json');
 
 test('check gendiff with json', () => {
-  expect(genDiff(fullFileJson, emptyFileJson)).toEqual(readFile('file1_emptyFile.txt'));
-  expect(genDiff(emptyFileJson, fullFileJson)).toEqual(readFile('emptyFile_file1.txt'));
-  expect(genDiff(fullFileJson, fullFileJson)).toEqual(readFile('file1_file1.txt'));
+  expect(genDiff(full1json, emptyFileJson)).toEqual(readFile('compare1_0.txt'));
+  expect(genDiff(emptyFileJson, full1json)).toEqual(readFile('compare0_1.txt'));
+  expect(genDiff(full1json, full2json)).toEqual(readFile('compare1_2.txt'));
+  expect(genDiff(full1json, full1json)).toEqual(readFile('compare1_1.txt'));
 });
 
-const fullFileYml = getFixturePath('file1.yml');
-const emptyFileYml = getFixturePath('emptyFile.yml');
+const full1yml = getFixturePath('full1.yml');
+const full2jyml = getFixturePath('full2.yml');
+const emptyFileyml = getFixturePath('empty.yml');
 
-test('check gendiff with yaml', () => {
-  expect(genDiff(fullFileYml, emptyFileYml)).toEqual(readFile('file1_emptyFile.txt'));
-  expect(genDiff(emptyFileYml, fullFileYml)).toEqual(readFile('emptyFile_file1.txt'));
-  expect(genDiff(fullFileYml, fullFileYml)).toEqual(readFile('file1_file1.txt'));
+test('check gendiff with yml', () => {
+  expect(genDiff(full1yml, emptyFileyml)).toEqual(readFile('compare1_0.txt'));
+  expect(genDiff(emptyFileyml, full1yml)).toEqual(readFile('compare0_1.txt'));
+  expect(genDiff(full1yml, full2jyml)).toEqual(readFile('compare1_2.txt'));
+  expect(genDiff(full1yml, full1yml)).toEqual(readFile('compare1_1.txt'));
 });
